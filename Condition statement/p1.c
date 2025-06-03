834be2256fd3ad83df40af01b0edba62c4a00bc4
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-03 18:57:33 +0530

    push

--- Condition statement/p1.c
+++ Condition statement/p1.c
@@ -1,13 +1,19 @@
-/*Chech Wether a number is positive*/
+/*Chech Wether a number is Positive Negative Neutral */
 #include<stdio.h>
 int main()
 {
     int num;
     printf("Enter a number :");
     scanf("%d",&num);
-    if(num > 0)
-    {
+    if(num > 0){
         printf("\n %d Number is Positive",num);
     }
+    else if(num < 0){
+        printf("\n %d Number is Negative",num);
+    }
+    else{
+        printf("\n %d Number is Neutral",num);
+    }
+    
     return 0;
 }
