761944eeeb2ea48c2f25e461ec481e2951f3e96f
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-03 19:39:26 +0530

    updates

--- Condition statement/Loops/loop1.c
+++ Condition statement/Loops/loop1.c
@@ -7,6 +7,12 @@ int main()
     scanf("%d",&num);
     for(i=1;i<=num;i++)
     {
-        printf(" %d",i);
+        printf("\n%d",i);
+    }
+    // Reverse print for loop
+    printf("\nReverse Print Loop");
+    for(i=num;i>=1;i--)
+    {
+        printf("\n%d",i);
     }
 }
