2f1e40129e4f0760552a85227d5ea84732831280
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-03 19:20:21 +0530

    update

---
+++ Condition statement/Loops/loop1.c
@@ -0,0 +1,12 @@
+/*Print  1 To N number*/
+#include<stdio.h>
+int main()
+{
+    int num,i;
+    printf("\n Enter The number :");
+    scanf("%d",&num);
+    for(i=1;i<=num;i++)
+    {
+        printf(" %d",i);
+    }
+}
