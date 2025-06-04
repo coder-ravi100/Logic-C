62126e2b7edc1c4f9d2a5c84152a49c63723d6b3
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-04 18:54:09 +0530

    updates

---
+++ Fuction/fun1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void sample()
+{
+    printf("\n Hello Mom And Dad");
+    
+}
+void family()
+{
+    printf("\n Hello Brother And Sister");
+}
+void dost()
+{
+    printf("\n Hello Kaival And Ruturaj");
+}
+int main()
+{
+    sample();
+    family();
+    dost();
+    return 0;
+}
