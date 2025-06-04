da4f6cccb81b4029eaa9aca69787396536e8cdc9
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-04 20:51:09 +0530

    updates

--- logic_building_program/Reverse_number.c
+++ logic_building_program/Reverse_number.c
@@ -14,3 +14,23 @@ int main()
     printf("\n %d The Reverse Number ",reversed);
     return 0;
 }
+/*Dry Run 
+remainder = num % 10;
+reversed = reversed * 10 +  remainder;
+num = num /10;
+--------------------------------------
+num = 123
+
+ 3 = 123 % 10
+ 13 = (0*10+3)
+ 12 = 123 / 10
+
+ 2  = 12 % 10
+ 12 = (1*10+2)
+ 1  = 12 / 10
+
+ 1  = 1 % 10
+ 31 = (3*10+1)
+ 0  = 0/10
+ Final = 321
+*/
