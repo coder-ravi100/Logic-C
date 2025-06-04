1f11281911e3a9301f66bd8b4c7133e7c94e159f
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-04 20:39:11 +0530

    updates

---
+++ logic_building_program/Sum-of-digits.c
@@ -0,0 +1,14 @@
+/*Sum Of Digits*/
+#include<stdio.h>
+int main()
+{
+    int num,digit=0;
+    printf("Enter the Number :");
+    scanf("%d",&num);
+    while(num!=0)
+    {
+        num = num  / 10;
+        digit++;
+    }
+    printf("\n Digits : %d",digit);
+}
