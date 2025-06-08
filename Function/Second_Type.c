607d8dbf402041d949d87ee7fa5b3a5ee1925456
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-08 13:10:09 +0530

    updates

--- Function/Second_Type.c
+++ Function/Second_Type.c
@@ -6,21 +6,57 @@ Function kuch return karega
 Lekin koi argument nahi lega*/
 #include<stdio.h>
 // Function with return but no argument
-int add()//blank Argument
+int add()
 {
-    int n1,n2,sum;
-    printf("\n Enter Number 1 :");//20
+    int n1,n2,result;
+    printf("\n Addition");
+    printf("\n Enter Number 1:");
     scanf("%d",&n1);
-    printf("\n Enter Number 2 :");//40
+    printf("\n Enter Number 2:");
     scanf("%d",&n2);
-    return sum = n1 + n2;
-    
+    return result = n1 + n2;
+}
+int sub()
+{
+    int n1,n2,result;
+    printf("\n Subtraction");
+    printf("\n Enter Number 1:");
+    scanf("%d",&n1);
+     printf("\n Enter Number 2:");
+    scanf("%d",&n2);
+    return result = n1 - n2;   
+}
+int mul()
+{
+    int n1,n2,result;
+    printf("\n Multiplication");
+    printf("\n Enter Number 1:");
+    scanf("%d",&n1);
+     printf("\n Enter Number 2:");
+    scanf("%d",&n2);
+    return result = n1 * n2;   
+}
+int div()
+{
+    int n1,n2,result;
+    printf("\n Division");
+    printf("\n Enter Number 1:");
+    scanf("%d",&n1);
+     printf("\n Enter Number 2:");
+    scanf("%d",&n2);
+    return result = n1 / n2;   
 }
 int main()
 {
-    int number;
-    number = add();//Blank Argument and call function
-    printf("\n Addition is :%d",number);
+    int num1,num2,num3,num4;
+    num1 = add();
+    num2 = sub();
+    num3 = mul();
+    num4 = div();
+    printf("\n The Addition Is : %d",num1);
+    printf("\n The Subtraction Is : %d",num2);
+    printf("\n The Multiplication Is : %d",num3);
+    printf("\n The Division Is : %d",num4);
     return 0;
 }
 /*Dry Run
--- Function/Third_Type.c
+++ Function/Third_Type.c
@@ -5,17 +5,6 @@ Function kuch return nahi karta (void use hota hai)
 Lekin function input/argument accept karta hai
 */
 #include<stdio.h>
-void checkEvenOdd(int num)
-{
-    if(num % 2!=0)
-    {
-         printf("\n %d Is Even",num);
-    } 
-    else
-    {
-         printf("\n %d Is Odd",num);
-    }
-}
 int main()
 {
     int number;
