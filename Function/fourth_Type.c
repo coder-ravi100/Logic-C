0d2ce716f6dfbe6c11f23f7f3fa62b9b1de971ee
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-10 18:03:25 +0530

    updates

---
+++ Function/fourth_Type.c
@@ -0,0 +1,14 @@
+/*Function Type 4:Without Return Type and Withouth Argument*/
+#include<stdio.h>
+void msg()//blank Argument
+{
+    printf("\n Hello World..");
+    printf("\n Bye Bye C Welcome C++");
+    /*No Return Any Value*/
+}
+int main()
+{
+    msg();
+    msg();
+    return 0;
+}
