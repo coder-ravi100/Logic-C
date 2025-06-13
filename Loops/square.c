2f03fb13d023a2b23e38d973d837ef8a5744f838
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-13 20:48:44 +0530

    updates

--- Loops/square.c
+++ Loops/square.c
@@ -1,1 +1,40 @@
 /*print squares of  1 to N number*/
+#include<stdio.h>
+int main()
+{
+    int num,sq,i,ans;
+    //meine sq ko num mein initilize kiya 
+    printf("\n Enter the Number:");
+    scanf("%d",&num);//10
+    for ( i = 1; i <=num; i++)
+    {
+       sq =  i * i;//1 4 9 16 25 
+       printf("\n%d Square :%d",i,sq); //i ka square nikala
+    }
+
+    //-----------------------------------------------
+      /* sq = num;  // num ki value sq mein daali
+       ans = sq * sq;  // square nikala
+       printf("%d",ans);*/
+       
+    //-----------------------------------------------
+     /*for ( sq = 1; sq <= num; sq++)
+        {
+            ans =  sq * sq;
+            printf("  %d",ans);
+        }*/
+    return 0;
+}
+/*Dry Run
+num = 10
+i=1 num = 10 True = i=1 * i=1 = 1
+i=2 num = 10 True = i=2 * i=2 = 4
+i=3 num = 10 True = i=3 * i=3 = 9
+i=4 num = 10 True = i=4 * i=4 = 16
+i=5 num = 10 True = i=5 * i=5 = 25
+i=6 num = 10 true = i=6 * i=6 = 36
+i=7 num = 10 True = i=7 * i=7 = 49
+i=8 num = 10 True = i=8 * i=8 = 64
+i=9 num = 10 True = i=9 * i=9 = 81
+i=10 num = 10 True = i=10 * i=10 = 100
+i=11 num = 10 False*/
--- logic_building_program/Armstrong.c
+++ logic_building_program/Armstrong.c
@@ -1,1 +1,47 @@
 /*Check The Armstrong Number*/
+
+//Armstrong Number Isse Number hote hai jiske cube ka sum diye hue number barabar ho
+
+/*Memory Trick to Remember:
+"Digit count karo → har digit ka power lo → add karo → compare karo*/
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num, digit = 0, sum = 0, rem = 0, power;
+    
+    printf("\nEnter the Number : ");
+    scanf("%d", &num);
+
+    int temp = num;
+    int copy = num;
+
+    // Step 1: Count digits
+    while (num != 0)
+    {
+        num = num / 10;
+        digit++;
+    }
+
+    // Step 2: Armstrong logic
+    while (temp != 0)
+    {
+        rem = temp % 10;
+        power = (int)(round(pow(rem, digit))); // fix here
+        sum = sum + power;
+        temp = temp / 10;
+    }
+
+    // Step 3: Compare
+    if (sum == copy)
+    {
+        printf("\n%d is an Armstrong Number\n", copy);
+    }
+    else
+    {
+        printf("\n%d is Not an Armstrong Number\n", copy);
+    }
+
+    return 0;
+}
