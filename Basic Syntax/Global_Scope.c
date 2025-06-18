6d6c66b754c8348238fc1101cc5b756e5241db11
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-18 20:41:17 +0530

    update

--- Basic Syntax/Global_Scope.c
+++ Basic Syntax/Global_Scope.c
@@ -2,11 +2,11 @@
 int a = 100; // Global variable
 
 void show() {
-    printf("a = %d\n", a); // ✅ Accessible here
+    printf("a = %d\n", a); //  Accessible here
 }
 
 int main() {
-    printf("a = %d\n", a); // ✅ Accessible here
+    printf("a = %d\n", a); //  Accessible here
     show();
     return 0;
 }
--- Basic Syntax/Local_Scope.c
+++ Basic Syntax/Local_Scope.c
@@ -1,4 +1,4 @@
-/*| Type               | Meaning (Hindi)                                                      | Use kaha hota hai |
+/*| Type               | Meaning                                                     | Use kaha hota hai |
 | ------------------ | -------------------------------------------------------------------- | ----------------- |
 | **Local Scope**    | Block ya function ke andar ka area                                   | Functions, loops  |
 | **Global Scope**   | Pure program mein kahin bhi use ho sakta hai                         | Function ke bahar |
@@ -8,7 +8,7 @@
 #include<stdio.h>
 int main() {
     int a = 10;  // Local to main
-    printf("%d", a); // ✅ Accessible here
+    printf("%d", a); //  Accessible here
     return 0;
 }
-// printf("%d", a); ❌ Error - a not declared in this scope
+// printf("%d", a);  Error - a not declared in this scope
